578c46616eda3b0e73c59e780cfd1946464d13fe
Ahesh-A <aheshalagu@gmail.com>
2024-05-15 19:55:46 +0530

    added code to test

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ export const app: Express = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("This is the front page")
 })
 
-app.get('/say-hello', (req, res) => {
+app.post('/say-hello', (req, res) => {
     const {name} = req.body
     res.send(`Hello ${name}`);
 })
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import {app} from '../src/index';
+import { response } from "express";
 // import test, { describe } from "node:test";
 
 describe("Test ==> root path", () => {
@@ -8,7 +9,24 @@ describe("Test ==> root path", () => {
         .get("/")
         .then(response => {
             expect(response.statusCode).toBe(200)
+            expect(response.text).toEqual("This is the front page")
+            console.log("Response: ", response.text)
             done();
         })
     })
 })
+
+describe("Test ==> print name", () => {
+    test("Should print hello with the passed name", async () => {
+        const payload = {
+            name: "ahesh"
+        }
+
+        const response = await request(app)
+        .post("/say-hello")
+        .send(payload);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toEqual(`Hello ${payload.name}`)
+        })
+    })
