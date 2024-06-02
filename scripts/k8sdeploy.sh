e6d8a0bde00a47a0ec893e651d89cfb52e578a21
Ahesh-A <aheshalagu@gmail.com>
2024-06-02 12:24:24 +0530

    changed k8sdeploy.sh

--- scripts/k8sdeploy.sh
+++ scripts/k8sdeploy.sh
@@ -1,3 +1,7 @@
-sed -e "s/\$CONTAINER_PORT/$CONTAINER_PORT/" \
-    -e "s/\$TARGET_PORT/$TARGET_PORT/" \
-    -e "s/\$K8SPORT/$K8SPORT/"  deployment.yaml | kubectl apply -f -
+CONTAINER_PORT_INT=$((CONTAINER_PORT))
+TARGET_PORT_INT=$((TARGET_PORT))
+K8SPORT_INT=$((K8SPORT))
+
+sed -e "s/\$CONTAINER_PORT/$CONTAINER_PORT_INT/" \
+    -e "s/\$TARGET_PORT/$TARGET_PORT_INT/" \
+    -e "s/\$K8SPORT/$K8SPORT_INT/"  deployment.yaml | kubectl --kubeconfig $KUBECONFIG apply -f -
